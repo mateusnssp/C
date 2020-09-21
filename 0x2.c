bd517ae1c6b7e94b9491398b5f015cb75ffb6c74
mateusnssp <mateusnss@protonmail.ch>
2020-09-21 17:06:27 -0300

    Create 0x2.c

---
+++ 0x2.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+/*
+        Fazer operações de matemática simples;
+*/
+
+int main(void){
+
+    int op;
+    float n1, n2, resultado;
+
+    printf("\nOPERAÇÔES BINÁRIAS\n1 --> Soma;\n2 --> Subtração;\n3 --> Multiplicação;\n4 --> Divisão.");
+    printf("\nInforme a operação que pretende realizar: \t");
+    scanf("%d", &op);
+    
+    if(op == 1){
+
+        printf("\nInforme o primeiro número: \t");
+        scanf("%f", &n1);
+        printf("\nInforme o primeiro número: \t");
+        scanf("%f", &n2);
+
+        resultado = n1 + n2;
+        printf("A soma entre %f e %f é %f\n", n1, n2, resultado);
+
+    }if(op == 2){
+
+        printf("\nInforme o primeiro número: \t");
+        scanf("%f", &n1);
+        printf("\nInforme o primeiro número: \t");
+        scanf("%f", &n2);
+
+        resultado = n1 - n2;
+        printf("A subtração entre %f e %f é %f\n", n1, n2, resultado);
+
+    }if(op == 3){
+
+        printf("\nInforme o primeiro número: \t");
+        scanf("%f", &n1);
+        printf("\nInforme o primeiro número: \t");
+        scanf("%f", &n2);
+
+        resultado = n1 * n2;
+        printf("A multiplicação entre %f e %f é %f\n", n1, n2, resultado);
+
+    }if(op == 4){
+
+        printf("\nInforme o primeiro número: \t");
+        scanf("%f", &n1);
+        printf("\nInforme o primeiro número: \t");
+        scanf("%f", &n2);
+
+        resultado = n1 / n2;
+        printf("A divisão entre %f e %f é %f\n", n1, n2, resultado);
+
+    }
+    return 0;
+}
