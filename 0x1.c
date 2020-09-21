2941304d41ac6e4a31b749ba91ed4063f7abdaff
mateusnssp <mateusnss@protonmail.ch>
2020-09-21 13:51:44 -0300

    Create 0x1.c

---
+++ 0x1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+
+    int num, sqrt_num;
+
+    printf("\nInforme um número: \t");
+    scanf("%d", &num);
+
+    sqrt_num = sqrt(num);
+    printf("A raíz quadrada de %d é %d.\n", num, sqrt_num);
+
+    return 0;
+
+}
