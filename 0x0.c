2d4224c061b12f6cc4fcaa8e217bc8db1083303b
Mateus Nunes <mateusnss@protonmail.ch>
2020-10-31 13:10:37 -0300

    Update 0x0.c

--- 0x0.c
+++ 0x0.c
@@ -2,31 +2,28 @@
 
 #include <stdio.h>
 
-int main(void){
-
+int main(void)
+{
     /*
             printf("%tipo_da_variável_var1 %tipo_da_variável_var2", var1, var2)
-
             % --> Imprime o caractere
-
             i --> Interio
             d --> Inteiro
             c --> Caracter
             f --> Ponto flutuante
     */
 
+    int num1, num2, sum;
 
-    int a, b, sum;
+    printf("Informe o primeiro número: \n");
+    scanf("%i", &num1);
+    printf("Informe o segundo número: \n");
+    scanf("%i", &num2);
 
-    printf("\nDefina um valor para a: \t");
-    scanf("%d", &a);
+    sum = num1 + num2;
+
+    printf("A dos dois números é igual a %i.\n", sum);
+
     
-    printf("\nDefina um valor para b: \t");
-    scanf("%d", &b);
-
-    sum = a + b;
-    printf("A soma entre a e b é %d.\n", sum);
-
     return 0;
-
 }
